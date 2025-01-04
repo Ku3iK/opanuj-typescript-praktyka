127102b9564ac8c0cd5aea01ed21a12cc95e3a60
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2025-01-04 14:18:14 +0100

    feat/421-literals-code

--- tasks/core-pro/421-literals-code/task.ts
+++ tasks/core-pro/421-literals-code/task.ts
@@ -1,5 +1,12 @@
-export type Code = null;
+type SingleCodeValue = 0 | 1;
+
+type CodeSegment = `${SingleCodeValue}${SingleCodeValue}${SingleCodeValue}`;
+
+export type Code = `${CodeSegment}-${CodeSegment}-${CodeSegment}`;
 
 export function codeToDecimal(code: Code) {
-  throw new Error('Not implemented');
+  const codeSegments = code.split('-');
+  const decimalSegments = codeSegments.map((code) => parseInt(code, 2));
+
+  return decimalSegments.join('');
 }
