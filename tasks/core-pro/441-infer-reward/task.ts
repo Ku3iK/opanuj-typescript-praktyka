b382d9c292f2d4aec6f8dbd2a5187962455c5901
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2025-01-26 16:51:33 +0100

    feat/441-infer-reward

--- tasks/core-pro/441-infer-reward/task.ts
+++ tasks/core-pro/441-infer-reward/task.ts
@@ -1,1 +1,8 @@
-export type RewardRadar = '🔍';
+export type RewardRadar<Scan extends string> =
+  Scan extends `${string}[${infer Reward}]${infer Rest}`
+    ? Reward extends `${infer N extends number}$`
+      ? N extends 0
+        ? RewardRadar<Rest>
+        : Reward
+      : RewardRadar<Rest>
+    : null;
