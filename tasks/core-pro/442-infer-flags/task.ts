a5d95170c8d738cfb75a4bd3e52a6158bc701ab1
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2025-01-26 17:23:29 +0100

    feat/442-infer-flags

--- tasks/core-pro/442-infer-flags/task.ts
+++ tasks/core-pro/442-infer-flags/task.ts
@@ -1,11 +1,17 @@
 import { type FeatureFlags } from './legacy-flags.ts';
 
-type FlagsV2<T> = {};
+type FlagsV2<T> = {
+  [Key in keyof T as Key extends `is${infer Feature}V2Enabled` ? Feature : never]: T[Key];
+};
 
 export type ModernFeatureFlags = FlagsV2<FeatureFlags>;
 
 export function getFeatureFlagsV2(flags: FeatureFlags): ModernFeatureFlags {
-  const flagsV2: ModernFeatureFlags = {};
+  const flagsV2: ModernFeatureFlags = Object.fromEntries(
+    Object.entries(flags)
+      .filter(([key]) => key.includes('V2'))
+      .map(([key, value]) => [key.replace('V2', ''), value]),
+  ) as ModernFeatureFlags;
 
   return flagsV2;
 }
